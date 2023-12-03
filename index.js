68426e4efe08f0849c072563c51cb6aa043c10d5
smithymmij <114240428+smithymmij@users.noreply.github.com>
2023-12-03 20:12:32 -0300

    Adção da rota mensagem

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',function(req,res)  {
     res.send('Olá Jimmy!')
 })
 
+app.get('/mensagem',function(req,res){
+    res.send('Essa mensagem é automática!')
+})
+
 app.listen(3000,function(){
     console.log("Conexão inicializada")
     
