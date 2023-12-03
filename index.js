c7c080147a3449cb8bbaf0e25254e2c46ec9808c
smithymmij <114240428+smithymmij@users.noreply.github.com>
2023-12-03 19:41:45 -0300

    index.js modificado de ola mundo para jimmy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',function(req,res)  {
-    res.send('Olá Mundo!')
+    res.send('Olá Jimmy!')
 })
 
 app.listen(3000,function(){
