62ed91a6975a2f0a76917227b59d420787d8fa82
smithymmij <114240428+smithymmij@users.noreply.github.com>
2023-12-03 21:54:50 -0300

    teste que deu errado

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/msg',function(req,res){
     res.send('Essa mensagem é automática!')
 })
 
+app.get('/sobre',function(req,res){
+    res.send('Esta página está sendo desenvolvida po Jimmy!')
+})
+
 app.listen(3000,function(){
     console.log("Conexão inicializada")
     
