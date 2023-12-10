95157ef06a61aed6f78a0bbb254e03656e959c70
smithymmij <114240428+smithymmij@users.noreply.github.com>
2023-12-10 20:44:47 -0300

    usando ejs nas paginas

--- index.js
+++ index.js
@@ -14,9 +14,17 @@ app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',function(req,res)  {
-    res.send('Olá Jimmy!')
+    res.render('index.ejs',{})
 })
 
+app.get('/usuarios',function(req,res)  {
+    res.render('usuarios.ejs',{usuarios:[
+        {nome:'jimmy',email:'mjimmysmith@gmail.com'},
+        {nome:'maria',email:'maria@gmail.com'},
+        {nome:'tereza',email:'tereza@gmail.com'},
+        {nome:'ana',email:'ana@gmail.com'}
+    ]})
+})
 
 app.listen(3000,function(){
     console.log("Conexão inicializada")
