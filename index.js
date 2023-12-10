4db19f18541ad9d7a7f4bfb473e7a719066fab92
smithymmij <114240428+smithymmij@users.noreply.github.com>
2023-12-10 19:18:19 -0300

    criação da rota Layout

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/msg',function(req,res){
     res.send('Essa mensagem é automática!')
 })
 
+app.get('/layout',function(req,res){
+    res.send('Esse é o novo layout!')
+})
+
 app.listen(3000,function(){
     console.log("Conexão inicializada")
     
