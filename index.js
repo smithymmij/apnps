511490d8e31ae22f6cbed8f4d3f50694819ce868
smithymmij <114240428+smithymmij@users.noreply.github.com>
2024-01-16 14:31:00 -0300

    finalizando aula

--- index.js
+++ index.js
@@ -12,15 +12,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+// Rota inicial
 app.get('/', async function (req, res) {
-    res.render('index.ejs', {});
+    try {
+        const usuarios = await Usuario.find({}).exec();
+        res.render('index.ejs', { Usuarios: usuarios });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro interno do servidor');
+    }
 });
 
+// Rota para exibir o formulário de adição de usuário
 app.get('/add', function (req, res) {
     res.render('adiciona.ejs');
 });
 
-app.post('/add', async function (req, res) {
+// Rota para processar a adição de um novo usuário
+app.post('/add', async function (req, res, next) {
     try {
         const usuario = new Usuario({
             nome: req.body.txtNome,
@@ -31,11 +40,58 @@ app.post('/add', async function (req, res) {
         await usuario.save();
         res.redirect('/');
     } catch (err) {
+        next(err);
+    }
+});
+
+// Rota para deletar um usuário
+app.get('/del/:id', async function(req, res, next) {
+    try {
+        await Usuario.findByIdAndDelete(req.params.id);
+        res.redirect('/');
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Rota para editar um usuário
+app.get('/edit/:id', async function(req, res, next) {
+    try {
+        const usuario = await Usuario.findById(req.params.id).exec();
+        res.render('edita.ejs', { Usuario: usuario });
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Rota para processar a edição de um usuário
+app.post('/edit/:id', async function (req, res) {
+    try {
+        const updatedUsuario = await Usuario.findByIdAndUpdate(
+            req.params.id,
+            {
+                nome: req.body.txtNome,
+                email: req.body.txtEmail,
+                senha: req.body.txtSenha,
+                foto: req.body.txtFoto
+            },
+            { new: true }
+        ).exec();
+
+        res.redirect('/');
+    } catch (err) {
         console.error(err);
         res.status(500).send('Erro interno do servidor');
     }
 });
 
+// Middleware de tratamento de erros
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Erro interno do servidor');
+});
+
+// Inicialização do servidor
 app.listen(3000, function () {
     console.log("Conexão inicializada");
 });
