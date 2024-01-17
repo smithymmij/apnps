22c79425d65251b62beeb69e91c0084db53de806
smithymmij <114240428+smithymmij@users.noreply.github.com>
2024-01-16 21:37:00 -0300

    curso finalizado

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const Usuario = require('./model/usuario');
+const upload = require('./config/configMulter')
+const bcrypt = require('bcrypt');
+
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -23,26 +26,44 @@ app.get('/', async function (req, res) {
     }
 });
 
+// Rota para filtro da tabela "g" para diferenciar maiuscula "i" para ignorar
+app.post('/', async function(req, res) {
+    try {
+        const searchTerm = req.body.txtPesquisa;
+        const regex = new RegExp(searchTerm, 'g');
+
+        const usuarios = await Usuario.find({ nome: { $regex: regex } }).exec();
+        res.render('index.ejs', { Usuarios: usuarios });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erro interno do servidor');
+    }
+});
+
 // Rota para exibir o formulário de adição de usuário
 app.get('/add', function (req, res) {
     res.render('adiciona.ejs');
 });
 
 // Rota para processar a adição de um novo usuário
-app.post('/add', async function (req, res, next) {
+app.post('/add', upload.single("txtFoto"), async function (req, res, next) {
     try {
+        const senhaCriptografada = await bcrypt.hash(req.body.txtSenha, 6);
+
         const usuario = new Usuario({
             nome: req.body.txtNome,
             email: req.body.txtEmail,
-            senha: req.body.txtSenha,
-            foto: req.body.txtFoto
+            senha: senhaCriptografada,
+            foto: req.file.filename
         });
+
         await usuario.save();
         res.redirect('/');
     } catch (err) {
         next(err);
     }
 });
+
 
 // Rota para deletar um usuário
 app.get('/del/:id', async function(req, res, next) {
@@ -65,15 +86,21 @@ app.get('/edit/:id', async function(req, res, next) {
 });
 
 // Rota para processar a edição de um usuário
-app.post('/edit/:id', async function (req, res) {
+app.post('/edit/:id', upload.single("txtFoto"), async function (req, res) {
     try {
+        let senhaCriptografada = req.body.txtSenha;
+
+        if (req.body.txtSenha) {
+            senhaCriptografada = await bcrypt.hash(req.body.txtSenha, 10);
+        }
+
         const updatedUsuario = await Usuario.findByIdAndUpdate(
             req.params.id,
             {
                 nome: req.body.txtNome,
                 email: req.body.txtEmail,
-                senha: req.body.txtSenha,
-                foto: req.body.txtFoto
+                senha: senhaCriptografada,
+                foto: req.file.filename
             },
             { new: true }
         ).exec();
@@ -84,6 +111,7 @@ app.post('/edit/:id', async function (req, res) {
         res.status(500).send('Erro interno do servidor');
     }
 });
+
 
 // Middleware de tratamento de erros
 app.use((err, req, res, next) => {
